09630cef487fdbcf5e61bca7a3f59cf870844c7b
Claiton B. Garcia <42419302+cbgprog@users.noreply.github.com>
2020-09-23 08:49:36 -0300

    Create exerc01.c
    Primeiro exercício da aula 01.

---
+++ Algoritmos e Programação II/Aula 01/exerc01.c
@@ -0,0 +1,34 @@
+/*
+ * Arquivo:     exerc01.c
+ * Data:        2020-09-16
+ * Disciplina:  Algoritmos e Programação II
+ * Descrição:   Primeiro exercício da aula 01.
+ * Autor:       Claiton B. Garcia <cbgprog@gmail.com>
+ * 
+ */
+
+#include <stdio.h>
+
+#define numElementos 4
+
+int main () {
+    
+    int vetorX[numElementos], vetorY[numElementos], x, produtoEscalar = 0;
+    
+    printf("Produto escalar de dois vetores.\n");
+
+    for (x = 0; x < numElementos; x++) {
+        printf("Digite o %iº valor do vetor X: ", x + 1);
+        scanf("%i", &vetorX[x]);
+    }
+    
+    for (x = 0; x < numElementos; x++) {
+        printf("Digite o %iº valor do vetor Y: ", x + 1);
+        scanf("%i", &vetorY[x]);
+        produtoEscalar += vetorX[x] * vetorY[x];
+    }
+    
+    printf("\nO produto escalar dos vetores X e Y é: %i\n\n", produtoEscalar);
+    
+    return 0;
+}
