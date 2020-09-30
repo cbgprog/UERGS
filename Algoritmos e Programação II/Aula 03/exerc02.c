c291e21947e0545f39953a293f53d3bb2edf70c8
Claiton B. Garcia <42419302+cbgprog@users.noreply.github.com>
2020-09-30 09:46:26 -0300

    Create exerc02.c
    Segundo exercício da Aula 03.

---
+++ Algoritmos e Programação II/Aula 03/exerc02.c
@@ -0,0 +1,36 @@
+/*
+ * Arquivo:     exerc01.c
+ * Data:        2020-09-30
+ * Disciplina:  Algoritmos e Programação II - Aula 03
+ * Descrição:   Implemente uma função recursiva para
+                computar o valor de 2^n.
+ * Autor:       Claiton B. Garcia <cbgprog@gmail.com>
+ * 
+ */
+
+#include <stdio.h>
+
+int potencia (int expoente) {
+    return expoente > 1 ? 2 * potencia(expoente - 1) : 2;
+}
+
+int main () {
+    
+    int expoente;
+    
+    printf("=== Potências ===\n");
+    
+    while (1) {
+        printf("Digite o expoente ou 0 para encerrar: ");
+        scanf("%i", &expoente);
+        if (expoente == 0) {
+            break;
+        }
+        
+        printf("2^%i = %i\n", expoente, potencia(expoente));
+    }
+    
+    printf("Até mais e obrigado pelos peixes!!\n");
+    
+    return 0;
+}
